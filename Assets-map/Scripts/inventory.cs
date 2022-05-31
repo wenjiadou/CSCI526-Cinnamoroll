76053f07efa09f96e8a7383a7b844f7288cf348c
wenjiadou <91001712+wenjiadou@users.noreply.github.com>
2022-05-30 17:30:42 -0700

    add the map of the tutorial scene with inventory

---
+++ Assets-map/Scripts/inventory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    // True if the slot is full, false if the slot is not full
+    public bool[] isFull;
+    // Slots that can hold help item
+    public GameObject[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

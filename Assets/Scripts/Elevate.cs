1daf134c9325512aa6cd8bb616ac1acea1b508ef
samr1104 <jruan@usc.edu>
2022-06-26 17:25:34 -0700

    Make elevate more smooth

--- Assets/Scripts/Elevate.cs
+++ Assets/Scripts/Elevate.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class Elevate : MonoBehaviour
 {
-    private RaycastHit hit;
+    // private RaycastHit hit;
     private GameObject player;
+    private float timeToMove = 0.5f;
     bool isUp = false;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isUp&&player.GetComponent<PlayerController>().isMoving == false){
-            // Debug.Log("test");
-            // if(Vector3.Distance(player.transform.position,transform.position + new Vector3(0,1,0))<0.1){
-            //     Debug.Log("player");
-            // }
-            if (Physics.Raycast(transform.position, Vector3.up, out hit, 1.2f) && hit.collider.CompareTag("Player")){
-                // elavete up
-                
-                transform.Translate(Vector3.up);
-                hit.collider.transform.Translate(Vector3.up);
-                isUp = true;
+        if (!isUp){
+            if(Vector3.Distance(player.transform.position, transform.position)<=1.1 && player.transform.position.y - transform.position.y == 1)
+            {
+                if (player.GetComponent<PlayerController>().isMoving == false){
+                    player.GetComponent<PlayerController>().isMoving = true;
+                    StartCoroutine(Move(Vector3.up,true));
+                    isUp=true;
+                    player.GetComponent<PlayerController>().isMoving = false;
+                }
             }
+        }
+        if (isUp && Vector3.Distance(player.transform.position, transform.position)>1.5){
+
+            
+                StartCoroutine(Move(Vector3.down,false));
+                // player.transform.Translate(Vector3.up);
+                isUp=false;
             
         }
+
+    }
+    private IEnumerator Move(Vector3 direction, bool withplayer) // move block and player, if withplayer == false , without moving player
+    {
+
+        float elapsedTime = 0;
+        Vector3 block_originPos = transform.position;
+        Vector3 block_targetPos = block_originPos + direction;   
+        Vector3 player_originPos = player.transform.position;
+        Vector3 player_targetPos = player_originPos + direction;
+        while(elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(block_originPos, block_targetPos, (elapsedTime / timeToMove));
+            if (withplayer) player.transform.position = Vector3.Lerp(player_originPos, player_targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = block_targetPos;
+        if (withplayer) player.transform.position = player_targetPos;
     }
 }

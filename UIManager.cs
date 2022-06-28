8139f797f3127591f7e50053bc3736a748586cbf
jlokkc <103162187+jlokkc@users.noreply.github.com>
2022-06-28 12:36:45 -0700

    Add files via upload
    update Scence and the UIManager script

---
+++ UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Text starsText;
+
+    private void Update()
+    {
+        UpdateStarsUI();//TODO Not put inside the Update method later
+    }
+
+    public void UpdateStarsUI()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            sum += PlayerPrefs.GetInt("Lv" + i.ToString());//Add the level 1 stars number, level 2 stars number.....
+        }
+
+        starsText.text = sum + "/" + 15;
+    }
+}
